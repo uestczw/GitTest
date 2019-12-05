3f7d02cebf0fa9d594503d79e8f766fe359b41dc
254809259@qq.com <admin@DESKTOP-N8Q2F9U>
2019-12-05 11:25:21 +0800

    adsasdasd

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -1,9 +1,12 @@
 package com.test;
 
+import java.util.UUID;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		UUID.randomUUID();
 		System.out.println("aaaaaaaaaaaa");
 	}
 
